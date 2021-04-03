cf80c10edf51337a35abdc3cf1ff1cceb282c054
Leonardo Hemerly <leohmcs@gmail.com>
2021-04-03 13:18:45 -0300

    Added pose and twist covariance.
    Added functions to calculate twist and pose covariance in order to fuse imu and wheel encoder data to estimate odometry. Also commented tf broadcast code to let robot_localization in charge of publishing tf.

--- scripts/odometry.py
+++ scripts/odometry.py
@@ -25,17 +25,29 @@ class Robot_Odom:
 
         self.vel_linear = 0.0
         self.vel_angular = 0.0
+        self.twist_covariance = [0, 0, 0, 0, 0, 0, 
+                                 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0]
 
         self.x = 0.0
         self.y = 0.0
         self.th = 0.0
+        self.pose_covariance = [0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0]
 
         # variacoes na posicao e orientacao desde a ultima publicacao
         self.delta_s = 0.0
         self.delta_th = 0.0
 
         self.odom_pub = rospy.Publisher("/odom", Odometry, queue_size=50)
-        self.odom_broadcaster = tf.TransformBroadcaster()
+        # self.odom_broadcaster = tf.TransformBroadcaster()
 
         self.right_joint_sub = rospy.Subscriber("/pioneer/right_wheel_joint_state", JointState, self.right_joint_callback)
         self.left_joint_sub = rospy.Subscriber("/pioneer/left_wheel_joint_state", JointState, self.left_joint_callback)
@@ -81,28 +93,85 @@ class Robot_Odom:
 
         self.th += self.delta_th 
 
+        self.pose_cov()
+        self.twist_cov()
+
         self.publish()
+
+
+    def pose_cov(self):
+        # Calculando a covariancia baeado na velocidade angular
+        # quando o robo esta girando, a covariancia e maior do que quando esta indo reto
+        if abs(self.vel_angular) > 0.2:
+            covariance_cons = 0.3
+        else:
+            covariance_cons = 0.05
+
+        self.pose_covariance[0] = covariance_cons
+        self.pose_covariance[7] = covariance_cons
+        self.pose_covariance[35] = 100 * covariance_cons
+
+        self.pose_covariance[1] = covariance_cons
+        self.pose_covariance[6] = covariance_cons
+
+        self.pose_covariance[31] = covariance_cons
+        self.pose_covariance[11] = covariance_cons
+
+        self.pose_covariance[30] = 10 * covariance_cons
+        self.pose_covariance[5] = 10 * covariance_cons
+
+        self.pose_covariance[14] = 0.1
+        self.pose_covariance[21] = 0.1
+        self.pose_covariance[28] = 0.1
+
+
+    def twist_cov(self):
+        # Calculando a covariancia baeado na velocidade angular
+        # quando o robo esta girando, a covariancia e maior do que quando esta indo reto
+        if abs(self.vel_angular) > 0.2:
+            covariance_cons = 0.3
+        else:
+            covariance_cons = 0.05
+
+        self.twist_covariance[0] = covariance_cons
+        self.twist_covariance[7] = covariance_cons
+        self.twist_covariance[35] = 100 * covariance_cons
+
+        self.twist_covariance[1] = covariance_cons
+        self.twist_covariance[6] = covariance_cons
+
+        self.twist_covariance[31] = covariance_cons
+        self.twist_covariance[11] = covariance_cons
+
+        self.twist_covariance[30] = 10 * covariance_cons
+        self.twist_covariance[5] = 10 * covariance_cons
+
+        self.twist_covariance[14] = 0.1
+        self.twist_covariance[21] = 0.1
+        self.twist_covariance[28] = 0.1
 
 
     def publish(self):
         odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.th)
 
-        self.odom_broadcaster.sendTransform (
-            (self.x, self.y, 0.),
-            odom_quat,
-            rospy.Time.now(), #self.current_time,
-            "base_link",
-            "odom"
-        )
+        # self.odom_broadcaster.sendTransform (
+        #     (self.x, self.y, 0.),
+        #     odom_quat,
+        #     rospy.Time.now(), #self.current_time,
+        #     "base_link",
+        #     "odom"
+        # )
 
         odom = Odometry()
         odom.header.stamp = self.current_time
         odom.header.frame_id = "odom"
 
         odom.pose.pose = Pose(Point(self.x, self.y, 0.), Quaternion(*odom_quat))
+        odom.pose.covariance = self.pose_covariance
 
-        odom.child_frame_id = "wheel_odom"
+        odom.child_frame_id = "base_link"
         odom.twist.twist = Twist(Vector3(self.vel_linear, 0, 0), Vector3(0, 0, self.vel_angular))
+        odom.twist.covariance = self.twist_covariance
 
         self.odom_pub.publish(odom)
 
